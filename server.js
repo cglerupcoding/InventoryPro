46ba4dbef0ae41ed7fbc25fc02074754b7174e82
chris <cglerup@gmail.com>
2019-06-15 00:48:26 -0500

    updated server.js file paths

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var passport   = require('passport')
 var session    = require('express-session')
 var bodyParser = require('body-parser')
 var env        = require('dotenv').config()
-var exphbs     = require('express-handlebars')
+var exphandlebars     = require('express-handlebars')
 
 
 
@@ -21,8 +21,8 @@ app.use(passport.session()); // persistent login sessions
 
  //For Handlebars
 app.set('views', './app/views')
-app.engine('hbs', exphbs({extname: '.hbs'}));
-app.set('view engine', '.hbs');
+app.engine('handlebars', exphandlebars({extname: '.handlebars'}));
+app.set('view engine', '.handlebars');
 
 
 app.get('/', function(req, res){
