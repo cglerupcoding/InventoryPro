4d411f95a441ece25083b2a973b6740409d7e0a3
chris <cglerup@gmail.com>
2019-06-29 14:53:03 -0500

    removed favicon

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var bodyParser   = require('body-parser');
 var cookieParser = require('cookie-parser');
 var express      = require('express');
 var exphbs       = require('express-handlebars');
-var favicon      = require('serve-favicon');
+
 var flash        = require('connect-flash');
 var logger       = require('morgan');
 var passport     = require('passport');
@@ -16,9 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-// Set up favicon, logging, parsing, static files
-// Uncomment after placing your favicon in public/images/
-//app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
